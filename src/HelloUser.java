84619f2d6d8c35f007a7d5b8ecb29d353b18a57f
Sweety Mohanty <sweetymohanty999@gmail.com>
2017-11-08 03:55:48 +0100

    HelloUser is added.

---
+++ src/HelloUser.java
@@ -0,0 +1,9 @@
+
+public class HelloUser {
+	 
+	public void greetUser (String name) {
+		System.out.println( "Hello "+ name + "!");
+		
+	}
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,25 @@
+import java.io.*;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		String firstName = "";
+		
+		InputStreamReader istream = new InputStreamReader(System.in) ;
+		BufferedReader bufRead = new BufferedReader(istream) ;
+		               
+		          try {
+		               System.out.println("Please enter your name: ");
+		               firstName = bufRead.readLine();
+		          }
+		          catch (IOException err) {
+		               System.out.println("Error reading line");
+		          }
+		      
+	HelloUser obj = new HelloUser();
+	obj.greetUser(firstName);
 	}
 
 }
