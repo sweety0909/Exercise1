5b6a8d2de889cde9e7e1124a7b490a04538fac30
Sweety Mohanty <sweetymohanty999@gmail.com>
2017-11-08 13:02:29 +0100

    "now" added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Hello ASE2017 - How are you now? :)");
 		
 		System.out.println("Please enter your name: ");
 		Scanner in = new Scanner(System.in);
